f9729213f077ec8fa121a3dcf9fa41e5f27aac82
wasehahmad <ahmadw@lafayette.edu>
2017-09-18 19:48:06 -0400

    Added deliverables

--- Lab03/Lab03.sim/sim_1/behav/elaborate.bat
+++ Lab03/Lab03.sim/sim_1/behav/elaborate.bat
@@ -1,6 +1,6 @@
 @echo off
 set xv_path=C:\\Xilinx\\Vivado\\2016.2\\bin
-call %xv_path%/xelab  -wto 83f6e1599ada4e8393d091507958fa27 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot rtl_transmitter_self_check_bench_behav xil_defaultlib.rtl_transmitter_self_check_bench xil_defaultlib.glbl -log elaborate.log
+call %xv_path%/xelab  -wto 83f6e1599ada4e8393d091507958fa27 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot mxtest_bench_behav xil_defaultlib.mxtest_bench xil_defaultlib.glbl -log elaborate.log
 if "%errorlevel%"=="0" goto SUCCESS
 if "%errorlevel%"=="1" goto END
 :END
