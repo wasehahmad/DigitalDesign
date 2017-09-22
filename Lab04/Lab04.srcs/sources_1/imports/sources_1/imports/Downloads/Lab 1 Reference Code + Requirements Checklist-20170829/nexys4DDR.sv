49f6e778c4c32c78096ea3ce56257bda07573dbe
ahmadw <ahmadw@ECE400-9SQXHH2.LAFAYETTE>
2017-09-22 16:41:54 -0400

    Added reg params for shifting data on nexys 7

--- Lab04/Lab04.srcs/sources_1/imports/sources_1/imports/Downloads/Lab 1 Reference Code + Requirements Checklist-20170829/nexys4DDR.sv
+++ Lab04/Lab04.srcs/sources_1/imports/sources_1/imports/Downloads/Lab 1 Reference Code + Requirements Checklist-20170829/nexys4DDR.sv
@@ -65,9 +65,11 @@ module nexys4DDR (
   // add SystemVerilog code & module instantiations here
     logic [7:0] data;
     logic [7:0] data_2;
+    logic [7:0] reg_0,reg_1,reg_2,reg_3;
+    logic rdy_pulse;
     
     assign transmitted = txd;  
-    
+    assign rdy = LED[0];
     
     debounce U_SEND_DEBOUNCE(.clk(CLK100MHZ), .button_in(BTNU), .pulse(debounced_send));
     debounce U_SEND_CONT(.clk(CLK100MHZ), .button_in(run), .button_out(cont_send));
@@ -76,10 +78,19 @@ module nexys4DDR (
     
     receiver_top U_RECEIVER_RTERM(.clk(CLK100MHZ), .reset(BTNC), .rxd(rxd), .rdy(LED[0]), .data(data), .ferr(LED[1]));
     
+    single_pulser U_RDY_PULSER (.clk(CLK100MHZ), .din(rdy), .d_pulse(rdy_pulse));
+    
+    reg_param #(.W(8)) U_D0(.clk(CLK100MHZ),.reset(BTNC),.lden(rdy_pulse),.d(data),.q(reg_0));
+    reg_param #(.W(8)) U_D1(.clk(CLK100MHZ),.reset(BTNC),.lden(rdy_pulse),.d(reg_0),.q(reg_1));
+    reg_param #(.W(8)) U_D2(.clk(CLK100MHZ),.reset(BTNC),.lden(rdy_pulse),.d(reg_1),.q(reg_2));
+    reg_param #(.W(8)) U_D3(.clk(CLK100MHZ),.reset(BTNC),.lden(rdy_pulse),.d(reg_2),.q(reg_3));
+    
+    
+    
     receiver_top U_RECEIVER_TXD(.clk(CLK100MHZ), .reset(BTNC), .rxd(txd), .rdy(rxd_rdy), .data(data_2), .ferr(ferr));
 
     dispctl U_DISPCTL(.clk(CLK100MHZ),.reset(BTNC),
-                    .d0(data[3:0]),.d1(data[7:4]),.d2(0),.d3(0),.d4(0),.d5(0),.d6(0),.d7(0),
+                    .d0(reg_0[3:0]),.d1(reg_0[7:4]),.d2(reg_1[3:0]),.d3(reg_1[7:4]),.d4(reg_2[3:0]),.d5(reg_2[7:4]),.d6(reg_3[3:0]),.d7(reg_3[7:4]),
                     .dp0(1),.dp1(1),.dp2(1),.dp3(1),.dp4(1),.dp5(1),.dp6(1),.dp7(1),
                     .seg(SEGS),.dp(DP),.an(AN));
 
