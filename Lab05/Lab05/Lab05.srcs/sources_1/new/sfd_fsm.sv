f739d154f7b1eb5884b5857841940015b1620590
watsongd <watsongd@ECE400-9SQXHH2.LAFAYETTE>
2017-10-24 22:05:45 -0400

    added data register

---
+++ Lab05/Lab05/Lab05.srcs/sources_1/new/sfd_fsm.sv
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/24/2017 09:34:02 PM
+// Design Name: 
+// Module Name: sfd_fsm
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sfd_fsm(
+    input preamble_detected,
+    input corroborating,
+    input sfd_detected,
+    input error,
+    output cardet,
+    output start_receiving
+    );
+endmodule
