377327b4f4fac2a98e8c1f53a8b18308c1c8e267
watsongd <watsongd@ECE400-9SQWHH2.LAFAYETTE>
2017-10-25 14:53:59 -0400

    framework for FSM, need to ask Waseh about timing

--- Lab05/Lab05/Lab05.srcs/sources_1/new/sfd_fsm.sv
+++ Lab05/Lab05/Lab05.srcs/sources_1/new/sfd_fsm.sv
@@ -21,11 +21,60 @@
 
 
 module sfd_fsm(
-    input preamble_detected,
-    input corroborating,
-    input sfd_detected,
-    input error,
-    output cardet,
-    output start_receiving
+    input logic clk,
+    input logic reset,
+    input logic preamble_detected,
+    input logic corroborating,
+    input logic sfd_detected,
+    input logic error,
+    output logic cardet,
+    output logic start_receiving
     );
+    
+    typedef enum logic[1:0] {
+        CHK_PREAMBLE=3'd0, CHK_SFD=3'd1, RECEIVING=3'd2
+    } states_t;
+    
+    states_t state, next;
+    
+    always_ff @(posedge clk) begin
+        if (reset) begin 
+            state <= CHK_PREAMBLE;
+        end
+        else begin
+            state <= next;
+        end
+    end
+    
+    always_comb begin
+    //defaults
+    cardet = 0;
+    next = CHK_PREAMBLE;    
+        
+        unique case (state)       
+            
+            CHK_PREAMBLE: begin
+                if (preamble_detected) begin 
+                    next = CHK_SFD;
+                    cardet = 1;
+                end
+                else next = CHK_PREAMBLE;
+            end
+            
+            
+            CHK_SFD: begin
+                if (sfd_detected) begin 
+                    next = RECEIVING;
+                    cardet = 1;
+                end
+                else next = CHK_SFD;
+            end
+           
+            RECEIVING: begin
+                if (error == 0) next = RECEIVING;
+                else next = RECEIVING;
+            end
+           
+        endcase
+    end
 endmodule
