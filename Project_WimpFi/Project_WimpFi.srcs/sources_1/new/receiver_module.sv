4df594c8e2fe9d2f8c7aca1e3b2bc728606a5926
ahmadw <ahmadw@ECE402-F6M8KB2.LAFAYETTE>
2017-11-11 20:15:43 -0500

    developed fsm for receiver. Also started building the receiver module

--- Project_WimpFi/Project_WimpFi.srcs/sources_1/new/receiver_module.sv
+++ Project_WimpFi/Project_WimpFi.srcs/sources_1/new/receiver_module.sv
@@ -21,19 +21,21 @@
 
 
 module receiver_module #(parameter BIT_RATE=50_000)(
-    input clk,
-    input reset,
-    input RXD,
-    input RRD,
-    input [7:0] MAC,
-    output cardet,
-    output RDATA,
-    output RRDY,
-    output RERRCNT,
-    output type_2_seen,
-    output ack_seen,
-    output [7:0] type_2_source
+    input logic clk,
+    input logic reset,
+    input logic RXD,
+    input logic RRD,
+    input logic [7:0] MAC,
+    output logic cardet,
+    output logic RDATA,
+    output logic RRDY,
+    output logic [7:0] RERRCNT,//might need to check the actual max err cnt
+    output logic type_2_seen,
+    output logic ack_seen,
+    output logic [7:0] type_2_source
     );
+    
+
     
     logic [7:0] data_received;
     logic data_byte_seen;
@@ -43,12 +45,48 @@ module receiver_module #(parameter BIT_RATE=50_000)(
     
     //manchester receiver
     man_receiver    #(.BIT_RATE(BIT_RATE))  U_MAN_RECEIVER(.clk(clk), .reset(reset), .rxd(RXD), .cardet(cardet), .data(data_received), .write(data_byte_seen), .error(error));
+    logic rxd_write_pulse;
+    single_pulser U_WRITE_PULSE(.clk(clk),.din(data_byte_seen),.d_pulse(rxd_write_pulse));
     
     //block RAM for RXD
+    assign write_en = rxd_write_pulse;
     blk_mem_gen_0 U_RXD_BRAM(.clka(clk),.addra(write_addr),.dina(data_received),.wea(write_en),.addrb(read_addr),.clkb(clk),.doutb(RDATA),.enb(read_en));
     
     
     ///////////////////////////////////////////////////////////////////////////CONTROL UNIT
+
+    logic [7:0] dest,pkt_type;
+    logic fcs_correct;
+    logic done_reading;
+    logic store_dest,store_type,store_src;
+    logic reset_receiver,incr_error;
+    
+
+    
+    //FSM for the control unit
+    rxd_fsm U_RXD_FSM(.clk(clk),.reset(reset),.byte_seen(rxd_write_pulse),.cardet(cardet),.destination(dest),.MAC_Addr(MAC),.pkt_type(pkt_type),.FCS_verified(fcs_correct),.all_bytes_received(done_reading),
+                    .reset_receiver(reset_receiver),.RRDY(RRDY),.type_2_seen(type_2_seen),.store_type(store_type),.store_dest(store_dest),.store_src(store_src),.incr_error(incr_error),.ACK_received(ack_seen));
+    
+    //flip flop to store data for fsm and transmitter
+    always_ff @(posedge clk)begin
+        if(reset)begin
+            RERRCNT <=0;
+            type_2_source <= 8'h00;
+            dest <=8'h00;
+            pkt_type<=8'h00; 
+        end
+        else begin
+            if(incr_error) RERRCNT<=RERRCNT+1;
+            if(store_src)type_2_source <=data_received;
+            if(store_dest)dest <=data_received;
+            if(store_type)pkt_type <=data_received;
+        end
+    
+    end
+    
+    //for the bram counter, only write dest,source,type,and data
+    //check type and adjust max of read counter when the data is to be read.e.g. max for type 0 = write_addr, else write_addr-1 to not read the fcs
+    
     
     
     
---
+++ Project_WimpFi/Project_WimpFi.srcs/sources_1/new/rxd_fsm.sv
@@ -0,0 +1,156 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Waseh Ahmad & Geoff Watson
+// 
+// Create Date: 11/11/2017 06:58:35 PM
+// Design Name: 
+// Module Name: rxd_fsm
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// FSM module for WimpFi System- Receiver Module
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module rxd_fsm(
+    input logic clk,
+    input logic reset,
+    input logic byte_seen,
+    input logic cardet,
+    input logic [7:0] destination,
+    input logic [7:0] MAC_addr,
+    input logic [7:0] pkt_type,
+    input logic FCS_verified,
+    input logic all_bytes_read,
+    output logic reset_receiver,
+    output logic RRDY,
+    output logic type_2_seen,
+    output logic store_type,
+    output logic store_dest,
+    output logic store_src,
+    output logic incr_error,
+    output logic ACK_received
+    );
+    
+    //global define for broadcast address (works similar to define in C)
+    `define BROADCAST_ADDR 8'H2A
+    `define TYPE_0 8'd0
+    `define TYPE_1 8'd1
+    `define TYPE_2 8'd2
+    `define TYPE_3 8'd3
+    
+    typedef enum logic[3:0]{
+        IDLE = 4'd0,STORE_DEST = 4'd1,STORE_SRC = 4'd2,STORE_TYPE = 4'd3,STORE_DATA = 4'd4,CHK_FCS = 4'd5,IGNORE = 4'd6,READING = 4'd7,DEST_CHK=4'd8
+    } states_t;
+    
+    states_t state, next;
+    
+    logic n_rrdy;
+    
+    always_ff @(posedge clk) begin
+        if(reset)begin
+            RRDY <=0;
+            state<=IDLE;
+        end
+        else begin
+            state<= next;
+            RRDY <=n_rrdy;
+        end
+    end
+    
+    always_comb begin
+        reset_receiver = 0;
+        n_rrdy=RRDY;
+        type_2_seen = 0;
+        store_type = 0;
+        store_dest = 0;
+        store_src = 0;
+        incr_error = 0;
+        ACK_received = 0;
+        
+        unique case (state) 
+            IDLE:begin
+                if(cardet)next=STORE_DEST;
+                else next = IDLE;
+            end
+            
+            STORE_DEST:begin
+                if(byte_seen)begin
+                    next = STORE_SRC;
+                    store_dest = 1;
+                end
+                else next = DEST_CHK;
+            end
+            
+            DEST_CHK:begin
+                if(destination == MAC_addr || destination == `BROADCAST_ADDR)next = STORE_SRC;
+                else next = IGNORE;
+            end
+            
+            STORE_SRC:begin
+                if(byte_seen)begin
+                    store_src = 1;
+                    next = STORE_TYPE;
+                end    
+                else next= STORE_SRC;
+            end
+            
+            STORE_TYPE:begin
+                if(byte_seen)begin
+                    store_type = 1;
+                    next = STORE_DATA;
+                end    
+                else next= STORE_TYPE;
+            end
+            
+            STORE_DATA:begin
+                if(cardet) next = STORE_DATA;
+                else begin
+                    if(pkt_type == `TYPE_0)next=READING;
+                    else next = CHK_FCS;
+                    n_rrdy = 1; //might need to change this to happen in reading
+                end
+            end
+            
+            CHK_FCS:begin
+                if(!FCS_verified)begin
+                    incr_error = 1;
+                    next = IDLE;
+                end
+                else begin
+                    case (pkt_type)
+                        `TYPE_2:if(destination ==`BROADCAST_ADDR)type_2_seen=1;
+                        `TYPE_3:ACK_received = 1;
+                    endcase
+                    next = READING;
+                end
+            end
+            
+            READING:begin
+                if(all_bytes_read)begin
+                    n_rrdy = 0;
+                    next = IDLE;
+                end
+                else next = READING;
+            end
+            
+            
+            
+        endcase
+    
+    
+    end
+    
+    
+    
+    
+    
+endmodule
