e5efacc1dfab25206fb6e1f4d215ed80d24ecd99
watsongd <watsongd@ECE400-9SQXHH2.LAFAYETTE>
2017-11-15 10:55:52 -0500

    updated tb

--- Project_WimpFi/Project_WimpFi.srcs/sim_1/new/txd_module_bench.sv
+++ Project_WimpFi/Project_WimpFi.srcs/sim_1/new/txd_module_bench.sv
@@ -20,26 +20,27 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module txd_module_bench(
-    input logic clk,
-    input logic reset,
-    input logic [7:0] XDATA,
-    input logic XWR,
-    input logic XSEND,
-    input logic cardet,
-    input logic type_2_seen,
-    input logic ACK_SEEN,
-    input logic [7:0] type_2_source,
-    input logic [7:0] MAC,
-    output logic XRDY,
-    output logic [7:0] ERRCNT,
-    output logic txen,
-    output logic txd
-    );
+module txd_module_bench;
+    logic clk;
+    logic reset;
+    logic [7:0] XDATA;
+    logic XWR;
+    logic XSEND;
+    logic cardet;
+    logic type_2_seen;
+    logic ACK_SEEN;
+    logic [7:0] type_2_source;
+    logic [7:0] MAC;
+    logic XRDY;
+    logic [7:0] ERRCNT;
+    logic txen;
+    logic txd;
     
     transmitter_module U_TXD_MOD(.clk(clk), .reset(reset), .XDATA(XDATA), .XWR(XWR), .XSEND(XSEND), 
                                  .cardet(cardet), .type_2_seen(type_2_seen), .ACK_SEEN(ACK_SEEN), 
                                  .MAC(MAC), .XRDY(XRDY), .ERRCNT(ERRCNT), .txen(txen), .txd(txd));
+    
+    logic [13:0] data = "THISISSOMEDATA";
                   
     always begin
         clk = 0;
@@ -47,15 +48,58 @@ module txd_module_bench(
         #5;
     end
     
-    //=================================== TYPE 0 ===================================//
+    //------------------------------------------------------
+    
     task send_one_byte;
-        
-    endtask;
+        XDATA = "T";
+        @(posedge clk) #1; 
+        XWR = 1;
+        @(posedge clk) #1;
+        XWR = 0;
+        @(posedge clk) #1;
+        XSEND = 1;
+        @(posedge clk) #1;
+        XSEND = 0;
+    endtask
+    
+    //------------------------------------------------------
+    
+    task send_multiple_bytes;
+        integer i;
+        for(i = 0; i < 10; i++) begin
+            XDATA = data[i];
+            @(posedge clk) #1; 
+            XWR = 1;
+            @(posedge clk) #1;
+            XWR = 0;
+            @(posedge clk) #1;
+        end
+        XSEND = 1;
+        @(posedge clk) #1;
+        XSEND = 0;
+    endtask
+    
+    //------------------------------------------------------
+
+    task 
+    
+    //=================================== TYPE 0 ===================================//
+    
+    task reset_case;
+        reset = 1;
+        repeat(10) @(posedge clk);
+        reset = 0;
+        check_ok("On reset, XRDY is high", XRDY, 1);
+        check_ok("On reset, XSND is low", XSND, 0);
+        check_ok("On reset, XWR is low", XWR, 0);
+        check_ok("On reset, XERRCNT is set to 0", XERRCNT, 0);
+    endtask
     
     initial begin
         reset = 1;
         repeat(10) @(posedge clk);
         reset = 0;
+        send_one_byte;
 
         $stop;
     end
