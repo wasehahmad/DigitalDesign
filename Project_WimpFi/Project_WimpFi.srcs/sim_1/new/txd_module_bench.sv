4c2ae00679eb6ac1549bc4dac13bb9003dc352ab
watsongd <watsongd@ECE400-9SQXHH2.LAFAYETTE>
2017-11-21 10:26:40 -0500

    updated tb

--- Project_WimpFi/Project_WimpFi.srcs/sim_1/new/txd_module_bench.sv
+++ Project_WimpFi/Project_WimpFi.srcs/sim_1/new/txd_module_bench.sv
@@ -38,12 +38,15 @@ module txd_module_bench;
     logic [7:0] XERRCNT;
     logic txen;
     logic txd;
+    logic [7:0] data;
     
     transmitter_module U_TXD_MOD(.clk(clk), .reset(reset), .XDATA(XDATA), .XWR(XWR), .XSEND(XSEND), 
                                  .cardet(cardet),/* .type_2_seen(type_2_seen), .ACK_SEEN(ACK_SEEN),.type_2_source(type_2_source)*/ 
-                                 .MAC(MAC), .XRDY(XRDY), .ERRCNT(XERRCNT), .txen(txen), .txd(txd));
+                                 .MAC(MAC), .XRDY(XRDY), .ERRCNT(XERRCNT), .txen(txen), .txd(txd)/*, .data(data)*/);
                                  
      assign XSEND = (XDATA ==8'h04 )&& XWR;//8'h04 is EOT or cntrl-D
+     
+     //SHREG 256 * 8 (2048) size.  compare data as it comes out. Needs enable signal
     
     logic [111:0] data = "ATADEMOSSISIHT";
                   
@@ -82,9 +85,8 @@ module txd_module_bench;
         XWR = 0;
         //wait a clock cycle
         @(posedge clk) #1;
-        //check_ok("After send is asserted, txen goes high", txen, 1); txen doesnt go high immediately after
-        while(txen==0)@(posedge clk);
-        while(txen==1)@(posedge clk);
+//        while(txen==0)@(posedge clk);
+//        while(txen==1)@(posedge clk);
     endtask
     
     //------------------------------------------------------
@@ -125,6 +127,7 @@ module txd_module_bench;
     
     task send_multiple_bytes;
         integer i;
+        while(!XRDY)@(posedge clk);
         XDATA = "*";//dest
         @(posedge clk) #1; 
         XWR = 1;
@@ -151,24 +154,22 @@ module txd_module_bench;
         XWR = 0;
         //wait a clock cycle
         @(posedge clk) #1;
-        check_ok("After send is asserted, txen goes high", txen, 1);
     endtask
     
     //------------------------------------------------------
     task wait_for_cardet;
-        while(!XRDY)@(posedge clk);
         cardet = 1;
         repeat(100) @(posedge clk);
         send_multiple_bytes;
-        repeat(100000) @(posedge clk);
+        repeat(10) @(posedge clk);
         cardet = 0;
         //cause a network busy in the middle of DIFS
         repeat(10) @(posedge clk);
         cardet = 1;
         repeat(10) @(posedge clk);
         cardet = 0;
-        while(txen==0)@(posedge clk);
-        while(txen==1)@(posedge clk);
+//        while(txen==0)@(posedge clk);
+//        while(txen==1)@(posedge clk);
         
     endtask
         
@@ -221,20 +222,68 @@ module txd_module_bench;
         reset = 1;
         @(posedge clk) #1;
         reset = 0;
-        //check outputs right after
+        //check outputs right after 
+        @(posedge clk) #1;
         check_ok("On reset, XRDY is high", XRDY, 1);
         check_ok("On reset, XSEND is low", XSEND, 0);
         check_ok("On reset, XWR is low", XWR, 0);
         check_ok("On reset, XERRCNT is set to 0", XERRCNT, 0);
     endtask
     
-    //When xrdy is high and xwr is asserted, a byte of data is sent to xdata
     task correct_transmission_one_byte;
         $display("===================================Testing Simulation test 2===================================");
+        //wait 100 clock cycles
+        reset = 0;
+        repeat(100)@(posedge clk);
+        //assert reset for one clock cycle
+        #1;
+        reset = 1;
+        @(posedge clk) #1;
+        reset = 0;
         send_one_byte_0;
-        //check_ok();
+        //after transmission
+        while(XRDY)@(posedge clk);
+        check_ok("XRDY goes low after transmission XSND is asserted", XRDY, 0);
+        while(!txen)@(posedge clk);
+        check_ok("TXEN goes high after sending a packet with one byte of data", txen, 1);
     endtask
     
+    task correct_transmission_multiple_bytes;
+        $display("===================================Testing Simulation test 3===================================");
+        //wait 100 clock cycles
+        reset = 0;
+        repeat(100)@(posedge clk);
+        //assert reset for one clock cycle
+        #1;
+        reset = 1;
+        @(posedge clk) #1;
+        reset = 0;
+        send_multiple_bytes;
+        //after transmission
+        while(XRDY)@(posedge clk);
+        check_ok("XRDY goes low after transmission XSND is asserted", XRDY, 0);
+        while(!txen)@(posedge clk);
+        check_ok("TXEN goes high after sending a packet with multiple bytes of data", txen, 1);
+    endtask    
+    
+    task correct_transmission_mutliple_bytes_with_backoff;
+        $display("===================================Testing Simulation test 4===================================");
+        //wait 100 clock cycles
+        reset = 0;
+        repeat(100)@(posedge clk);
+        //assert reset for one clock cycle
+        #1;
+        reset = 1;
+        @(posedge clk) #1;
+        reset = 0;
+        wait_for_cardet;
+        //after transmission
+        while(XRDY)@(posedge clk);
+        check_ok("XRDY goes low after transmission XSND is asserted", XRDY, 0);
+        while(!txen)@(posedge clk);
+        check_ok("TXEN goes high after sending a packet with multiple of data and a busy network", txen, 1);
+    endtask
+        
     initial begin
         reset = 1;
         cardet = 0;
@@ -245,16 +294,18 @@ module txd_module_bench;
         repeat(10) @(posedge clk);
         reset = 0;
         reset_case;
-        send_one_byte_0;
+        correct_transmission_one_byte;
+        correct_transmission_multiple_bytes;
+        correct_transmission_mutliple_bytes_with_backoff;
         repeat(1000)@(posedge clk);
- //       $stop;
+//        $stop;
 //        send_one_byte_0;
 //        wait_for_cardet;
 //        test_watchdog;
 //        MAC="#";
-        send_one_byte_1;
-        repeat(1000)@(posedge clk);
-        send_one_byte_1;
+//        send_one_byte_1;
+//        repeat(1000)@(posedge clk);
+//        send_one_byte_1;
         $stop;
     end
 endmodule
